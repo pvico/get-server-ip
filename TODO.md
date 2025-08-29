2c25605e3dba587723846881ecfb3ce3735bcdf9
Philippe Vico <philippe@pvico.net>
2025-08-29 20:51:05 +0200

    TODO.md

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+1. Pass the Config object to get_ip.getIp
+2. Remove the constants.zig file
+3. Put a dummy config.json in the main directory for example on Github
--- src/constants.zig
+++ src/constants.zig
@@ -1,3 +1,6 @@
+// TODO: Remove this file 
+// These constants will come from the JSON file 
+
 pub const GIST_URI = "https://gist.githubusercontent.com/pvico/afae39ce638c777decaf6ee2d938a04c/raw/f61a6249bd2c4c00f0d95d49213a70adcbc8cdc7/wanip.txt";
 pub const DDNS_DOMAIN_1 = "cptvico.ddns.net";
 pub const DDNS_DOMAIN_2 = "cptvico.mywire.org";
